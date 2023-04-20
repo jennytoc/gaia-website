0ac420be944ed62e48058b4363c786e80b33b4a1
jennytoc <toctocanj@gmail.com>
2023-04-19 20:51:15 -0600

    Started adding elements to index.js

---
+++ gaia-app-frontend/src/pages/index.js
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <nav></nav>
+      <div className="carousel">
+      
+      </div>
+      <div className="categories">
+
+      </div>
+      <section className='designers'>
+
+      </section>
+      <section className='donation'>
+
+      </section>
+      <section className='community'>
+
+      </section>
+      <footer>
+        
+      </footer>
+    </main>
+  )
+}
