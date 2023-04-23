99a76a3dae42ce7b76e4356b94586e07d6bde7b2
jennytoc <toctocanj@gmail.com>
2023-04-22 18:50:08 -0600

    Added carousel functionality and images

--- gaia-app-frontend/src/components/carousel.js
+++ gaia-app-frontend/src/components/carousel.js
@@ -1,22 +1,55 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import Image from 'next/image'
+
 import 'swiper/css';
+import "swiper/css/pagination";
 
 export default function Carousel() {
   return (
-    <Swiper>
-      <SwiperSlide>1</SwiperSlide>
-      <SwiperSlide>2</SwiperSlide>
-      <SwiperSlide>3</SwiperSlide>
+    <Swiper
+      modules={[Pagination]}
+      slidesPerView={1}
+      pagination={true}
+    >
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={500} height={500}/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/pula.png" width={500} height={500}/>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={500} height={500}/>
+      </SwiperSlide>
     </Swiper>
   );
 }
 
 export function Categories() {
   return (
-    <Swiper>
-      <SwiperSlide>1</SwiperSlide>
-      <SwiperSlide>2</SwiperSlide>
-      <SwiperSlide>3</SwiperSlide>
+    <Swiper
+      slidesPerView={4}
+    >
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={100} height={100}/>
+        <p>MEN</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={100} height={100}/>
+        <p>WOMEN</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={100} height={100}/>
+        <p>SALE</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={100} height={100}/>
+        <p>TOPS</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/images/grey.png" width={100} height={100}/>
+        <p>BOTTOMS</p>
+      </SwiperSlide>
   </Swiper>
   )
 }
--- gaia-app-frontend/src/pages/index.js
+++ gaia-app-frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 import Carousel from '@/components/carousel.js'
 import { Categories } from '@/components/carousel.js'
 
@@ -20,21 +21,62 @@ export default function Home() {
         <Carousel />
       </div>
 
-      <div className="categories">
+      <div id="categories">
         <Categories />
       </div>
-      
-      <section className='designers'>
 
+      <section id='designers'>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h4>GAIA X</h4>
+          <h5>Designer Name</h5>
+          <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+          <button>Shop</button>
+        </div>
       </section>
-      <section className='donation'>
 
+      <section id='donation'>
+        <div>
+          <h4>DONATION DISCOUNTS</h4>
+          <p>Donate now and get 15% off your next purchase!</p>
+          <button>Donate</button>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
       </section>
-      <section className='community'>
 
+      <section id='community'>
+        <h4>THE GAIA COMMUNITY</h4>
+        <div>
+          <img src="" alt="" />
+          <h6>Connect</h6>
+          <p>with eco-conscious individuals and like-minded people</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h6>Share</h6>
+          <p>your sustainable values and experiences</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h6>Post</h6>
+          <p>photos of your new sustainable wardrobe</p>
+        </div>
+        <button>Forums</button>
       </section>
+
       <footer>
-
+        <h4>GAIA</h4>
+        <ol>
+          <li>My Account</li>
+          <li>About GAIA</li>
+          <li>Sustainability Info</li>
+          <li>Get Help</li>
+        </ol>
+        <small>2023 GAIA All Rights Reserved</small>
       </footer>
     </main>
   )
